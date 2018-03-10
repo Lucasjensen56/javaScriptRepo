d25b174094e06aedcdd2ce9a9dca1adc35208c4f
Lucas Jensen <lucasjensen@er-10-132-207-67.wireless.umn.edu>
2018-03-10 13:14:18 -0600

    added a few for loops and functions

--- examples.js
+++ examples.js
@@ -1,2 +1,24 @@
 "use strict";
 
+const testArray = [1, 2, 3, 4];
+// console.log(testArray[0]);
+
+for (let i = 0; i <= testArray.length; i++) {
+// console.log(i);
+}
+
+let j = 0;
+while (j < testArray.length) {
+// console.log(testArray[j]);
+j++ 
+
+}
+
+let newArray = []
+testArray.forEach(function(element) {
+const result = element + 1;
+newArray.push(result)
+console.log(result);
+}); 
+
+console.log(newArray);
